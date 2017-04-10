bb3471838d49d6e31e967e4a1d87ea3040f04002
saadkhan6670 <saad.khan6670@gmail.com>
2017-04-10 03:03:17 -0700

    Modified

--- user/model.js
+++ user/model.js
@@ -1,12 +1,29 @@
 'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var validate = require('mongoose-validator');
+
+var emailValidate =[
+    validate({
+        validator : 'matches',
+        arguments:/^[\w.]+[@]+[a-zA-Z]+.com$/,
+        message:'Must be a valid email'
+    })
+];
+
+var phoneValidate =[
+    validate({
+        validator : 'matches',
+        arguments:/^923[1-9]{9}$/,
+        message: ' must be a valid phone '
+    })
+];
 
 var UserSchema = new Schema ({
     firstname : String,
     lastname : String,
-    email : { type:String, unique: true },
-    phone : Number,
+    email : { type:String, unique: true, lowercase:true, validate:emailValidate },
+    phone : {type:Number, validate:phoneValidate},
     password : String
 
 });
